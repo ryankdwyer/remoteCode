00a2378b2b8116fcfa1e27d13649eeb543648562
Ryan Dwyer <ryankdwyer@gmail.com>
2015-09-27 13:11:15 -0400

    Some minor changes to html and css to add responsiveness

--- browser/js/video/video.controller.js
+++ browser/js/video/video.controller.js
@@ -21,7 +21,7 @@ app.controller('VideoController', function ($scope, $state, VideoFactory) {
         debug: false,
         detectSpeakingEvents: true,
         autoAdjustMic: false,
-        url: "http://localhost:8888/",
+        url: "http://192.168.1.15:8888/",
     });
 
     // when it's ready, join if we got a room from the URL
